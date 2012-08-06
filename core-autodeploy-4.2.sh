45bc12bfc0b694e857f441ca66d4fcdb5e1505de
Daniel Robbins <drobbins@funtoo.org>
2012-08-06 15:19:37 +0000

    another lil fix

--- core-autodeploy-4.2.sh
+++ core-autodeploy-4.2.sh
@@ -45,9 +45,9 @@ arch="x86_64"
 mysql_ftp_mirror="ftp://mirror.anl.gov/pub/mysql/Downloads/MySQL-5.5/"
 
 # Auto-detect latest build:
+build=4.2.0
 zenoss_base_url="http://downloads.sourceforge.net/project/zenoss/zenoss-4.2/zenoss-$build"
 zenpack_base_url="http://downloads.sourceforge.net/project/zenoss/zepacks-4.2/zenpacks-$build"
-build=4.2.0
 zenoss_rpm_file="zenoss-$build.$els.$arch.rpm"
 zenpack_rpm_file="zenoss-core-zenpacks-$build.$els.$arch.rpm"
 
