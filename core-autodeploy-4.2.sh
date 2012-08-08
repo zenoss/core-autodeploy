bab1b7b17d281cddf4743b69a3e9faa37406f504
Daniel Robbins <drobbins@funtoo.org>
2012-08-08 20:15:40 +0000

    little regex tweak

--- core-autodeploy-4.2.sh
+++ core-autodeploy-4.2.sh
@@ -90,7 +90,7 @@ if [ -e /etc/selinux/config ]; then
 	sed -i -e 's/^SELINUX=.*/SELINUX=disabled/g' /etc/selinux/config
 fi
 
-openjdk="$(rpm -qa | grep java*openjdk)"
+openjdk="$(rpm -qa | grep java.*openjdk)"
 if [ -n "$openjdk" ]; then
 	echo "Attempting to remove existing OpenJDK..."
 	try rpm -e $openjdk
